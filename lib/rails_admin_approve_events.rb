f216796db9077d0484b6031023c44fd3869f03ea
sungil lim <chamon1231@hotmail.com>
2013-03-08 12:20:28 +0800

    'update'

--- lib/rails_admin_approve_events.rb
+++ lib/rails_admin_approve_events.rb
@@ -8,24 +8,28 @@ module RailsAdmin
       class ApproveEvents < RailsAdmin::Config::Actions::Base
 				RailsAdmin::Config::Actions.register(self)
 
+        register_instance_option :visible? do
+          bindings[:abstract_model].to_s == "Event"
+        end
         register_instance_option :http_methods do
           [:get, :post]
         end
 
 				register_instance_option :controller do
 					Proc.new do
-
             if request.post? 
-              @objects = list_entries(@model_config, :destroy)
-              render @action.template_name
+              @objects = list_entries(@model_config, :approve_events)
+              @objects.each do |object|
+                object.update_attribute(:approved, true)
+              end  
+              flash[:notice] = "You have approved #{@objects.count} event(s)."
+              redirect_to back_or_index
             else
               render @action.template_name
             end
           end
 				end
 				register_instance_option :bulkable? do
-          parent_model = bindings[:abstract_model].try(:config).try(:parent)
-          puts parent_model
           true
         end			 
 				register_instance_option :link_icon do
